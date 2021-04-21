855048078ce9728737af2c57b767b13298b3ee6e
Wellington Araujo <welles.araujo@hotmail.com>
2021-04-21 14:55:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './estilo.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Biscoito from './assets/biscoito.png'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      frase:''
+    }
+    this.frases=[
+      'Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ]
+    this.quebraBiscoito=this.quebraBiscoito.bind(this)
+    }
+
+    quebraBiscoito(){
+      let newState = this.state;
+      let randomNumber = Math.floor(Math.random()*this.frases.length)
+      newState.frase = '"'+this.frases[randomNumber]+'"';
+      this.setState({state:newState})
+    }
+
+  
+  render(){
+    return(
+      <div className="container">
+          <img className="img"src={Biscoito}/>
+          <Botao nome='Abrir biscoito' acaoBtn = {this.quebraBiscoito}/>
+          <h3 className="frase">{this.state.frase}</h3>
+
+      </div>
+    )
+  }
+}
+
+class Botao extends Component {
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
